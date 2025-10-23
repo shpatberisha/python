938806e3ed346358c51c8b37d8fcd292fd18f79d
shpatberisha <shpatberisha2510@gmail.com>
2025-10-23 16:18:13 +0200

    Add files via upload

---
+++ moduli1/index.py
@@ -0,0 +1,12 @@
+# print('Hello World')
+
+shopping_list=['apples','oranges',3.5,4.5]
+
+print(shopping_list[2])
+
+shopping_list.append('milk')
+shopping_list.insert(2,'lemons')
+
+shopping_list.remove(3.5)
+shopping_list.pop(1)
+print(shopping_list)
