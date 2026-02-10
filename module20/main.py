c82140c99376a1b5131cac3993e02f465c5b1a25
shpat <shpatberisha2510@gmail.com>
2026-02-10 16:02:51 +0100

    yeeyfdkjwd

---
+++ module20/main.py
@@ -0,0 +1,51 @@
+# {
+#     "name":"Alice",
+#     "age":25,
+#     "city":"Madridi"
+# }
+#
+# {
+#     "name":"John Doe",
+#     "age":30,
+#     "adress":{
+#         "street":"123 Main St",
+#         "city":"Prishtine",
+#         "state":"Kosove",
+#         "PostCode":"10000"
+#     },
+#     "contacts":[
+#         {
+#             "type":"Email",
+#             "value":"johndoe@Gmail.com"
+#         },
+#         {
+#             "type":"phone",
+#             "value":"444-122-4567"
+#         }
+#
+#
+#     ]
+#
+#
+#
+#
+# }
+
+
+{
+    "fruits"[
+        {
+            "name":"Apple",
+            "color":"#FF0000",
+            "details":{
+                "type":'Pome',
+                "season":'Fall'
+            },
+            "nutrients":{
+                'colories':52,
+                'fiber':"2.4g",
+                "vitaminC":"4.6mg"
+            }
+        }
+    ]
+}
